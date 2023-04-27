6f5cbf433fdbedaa08d734b1f57a2d537e75642a
Arya A S <80664071+arya-ashok98@users.noreply.github.com>
2023-04-27 15:43:15 +0530

    Add files via upload

---
+++ MySQL lab2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `mysql labs`.`userdetails` (`userId` INT NOT NULL AUTO_INCREMENT , `userName` VARCHAR(10) NOT NULL , `place` VARCHAR(30) NOT NULL , PRIMARY KEY (`userId`)) ENGINE = InnoDB;
+
+CREATE TABLE `mysql labs`.complaints (userId INT, description VARCHAR(255) NOT NULL, FOREIGN KEY(userId) REFERENCES userdetails(userId));
+
+INSERT INTO userdetails (userName, place) VALUES ('Anu', 'Kollam'), ('Alan', 'Kottayam'), ('Rahul', 'Kollam');
+
+INSERT INTO complaints(userId, description) VALUES (1, 'The site is getting too slow for me from this morning, Could you please check the same! Thanks!!'), (3, 'Im unable to login to the application. Showing access denied message! Please check and get back urgently');
